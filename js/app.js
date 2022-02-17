5e3ff55dbae581296854ba1aabaf6a6043776861
alemica97 <alessandro.micalizzi08@gmail.com>
2022-02-17 17:16:55 +0100

    prove sul funzionamento degli oggetti

---
+++ js/app.js
@@ -0,0 +1,21 @@
+//copiare la parte HTML in JS usando il template Literal
+
+
+//creo un singolo oggetto come prova e lo stampo con console.log
+// const teamMember = {
+//     name : 'Weyne Barnett',
+//     role : 'Founder & CEO' 
+// }
+
+// console.log(teamMember.name);
+
+// const card = document.querySelector('.team-container');
+// console.log(card.innerHTML);
+
+// card.innerHTML = `<div class="card-text">
+// <h3>${teamMember['name']}</h3>
+// <p>Founder & CEO</p>
+// </div>`
+
+//creo un array composto da più oggetti
+//ogni oggetto rappresenta un membro del team
